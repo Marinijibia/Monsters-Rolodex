4b06303c4c1700c356b078b8fc583065478861b3
Umar Falalu <marinijibiaa@gmail.com>
2023-02-04 21:55:15 +0100

    Using class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component  } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((Response) => Response.json())
+    .then((users) => this.setState(() =>{return {monsters: users}}, ()=>{console.log(this.state)}))
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <input className="search-box" type="search" placeholder="search monsters" onChange={(event) =>{
+          const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(
+              event.target.value.toLocaleLowerCase()
+            );
+          });
+
+          this.setState(() =>{
+            return {monsters: filteredMonsters}
+          }, () =>{
+            console.log(this.state)
+          })
+        }}/>
+         {this.state.monsters.map((monster) => {
+          return (
+            <div>
+              <h3>{ monster.name}</h3>
+            </div>
+          )
+         })}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
